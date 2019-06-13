906e00c7cc9b8ac4e54cf70ca30eb925f5b1d1e3
Chris <c.lamothe25@gmail.com>
2019-06-13 19:20:09 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = "Chris PHP";
+$greeting = 'Hello, PHP Universe';
+// echo $greeting;
+echo "Hello $name <br>";
+echo "Hi, " . $name . "<br>";
+$first = "So far,";
+$second = "So good";
+echo "$first $second";
+
